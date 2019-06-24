7d424d4705aec29e6b7340cd7f796cd1521ffe3d
Praveena <praveena@spuriq.com>
2019-06-23 17:15:09 -0700

    Switch Imge implemented

--- TestAddImages/ViewController.swift
+++ TestAddImages/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    var showFirstImage = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func switchButtonClicked(_ sender: Any) {
+        if (showFirstImage) {
+            myImageView.image = UIImage(named: "Psyduck")
+            showFirstImage = false
+        } else {
+            myImageView.image = UIImage(named: "Rubix")
+            showFirstImage = true
+        }
+    }
+    
 }
 
